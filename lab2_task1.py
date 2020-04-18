4913dfdfcd7907331bd45281e60ea661dbb7d2a6
nastassya-sav <61316882+nastassya-sav@users.noreply.github.com>
2020-04-18 19:33:43 +0300

    ready

--- lab2_task1.py
+++ lab2_task1.py
@@ -1,15 +1,18 @@
 import math
 
+
 def get_choice():
     while True:
         choose = input('0. Input data from file\n1. '
-                           'Input data from keyboard\n"exit" to exit\n')
+                       'Input data from keyboard\n'
+                       '"exit" to exit\n')
         if choose in ('0', '1', 'exit'):
             break
         else:
             print('Please, enter the right command')
-    
+
     return choose
+
 
 def keyboard_option():
     while True:
@@ -19,11 +22,12 @@ def keyboard_option():
                 continue
 
             arr = [int(input('Input integer number ')) for i in range(n)]
-            
+
             return arr
 
         except ValueError:
             print('Mistake. Input integer numbers, please')
+
 
 def file_option():
     arr = []
@@ -38,6 +42,7 @@ def file_option():
 
     return arr
 
+
 def main():
     while True:
         choice = get_choice()
@@ -48,7 +53,7 @@ def main():
             arr = keyboard_option()
         else:
             break
-        
+
         n = len(arr)
         print(arr)
         length = math.ceil(math.sqrt(n))  # block length
@@ -64,7 +69,8 @@ def main():
             if i % length == length - 1:  # check whether the block is over
                 arr_sum.append(temp_sum)  # add the current sum
                 # to the array of sums
-                temp_sum = 0  # zero the current amount to check the next block
+                temp_sum = 0  # zero the current amount to check the next
+                # block
             elif i == n - 1:  # if not the end of the block, but the last
                 # element of the array
                 arr_sum.append(temp_sum)  # add the sum of the defective block
@@ -88,8 +94,10 @@ def main():
         block_left = left // length  # block number for the left border
         block_right = right // length  # block number for the right border
         result = 0
-        if block_right - block_left < 2:  # when there are no blocks between them
-            for k in range(left, right + 1): # range does not include upper bound
+        if block_right - block_left < 2:  # when there are no blocks between
+            # them
+            for k in range(left, right + 1):  # range does not include upper
+                # bound
                 result += arr[k]
         else:
             # summarize from the next block,
@@ -100,54 +108,14 @@ def main():
             # (block_left + 1) * length - position of the first element
             # of the next block(is not included)-
             # count remaining parts
-            for k in range(left, (block_left + 1) * length):  # left remaining part
+            for k in range(left, (block_left + 1) * length):  # left remaining
+                # part
                 result += arr[k]
-            for k in range(block_right * length, right + 1):  # right remaining part
+            for k in range(block_right * length, right + 1):  # right
+                # remaining part
                 result += arr[k]
         print(result)
 
+
 if __name__ == "__main__":
     main()
-
-"""def main():
-    while True:
-        choose = input('0. Input data from file\n1. '
-                           'Input data from keyboard\n"exit" to exit\n')
-        if choose == 'exit':
-            break
-        else:
-            incorrect_input = True
-
-            while incorrect_input:
-                try:
-                    choose = int(choose)
-                    incorrect_input = False  # when entered int
-                except ValueError:
-                    incorrect_input = True
-                    print('Input integer number, please')
-                if choose != 0 and choose != 1 and choose != "exit":
-                    print('Number must be 0 or 1 or "exit"')
-                    incorrect_input = True
-        arr = []
-        if choose == '1':  # enter data from the keyboard
-            incorrect_input = True
-            while incorrect_input:
-                try:
-                    n = int(input('Input size of array (has to be positive): '))
-                    arr = [int(input('Input integer number ')) for i in range(n)]
-                    incorrect_input = False
-                    if n <= 0:
-                        incorrect_input = True
-                except ValueError:
-                    incorrect_input = True
-                    print('Mistake. Input integer numbers, please')
-        else:
-            f = open('lab_1.txt', 'r')  # open the text file in read mode
-            for line in f:  # go through the lines
-                array_with_numbers = line.split()  # splits a line into numbers
-                # according to spaces
-                for i in range(len(array_with_numbers)):
-                    arr.append(int(array_with_numbers[i]))  # add numbers
-                    # to the array
-            f.close()
-"""
